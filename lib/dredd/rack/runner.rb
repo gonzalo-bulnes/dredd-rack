357470a9d13f9360689843de78008c6f876ae48b
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-03-04 01:03:02 -0300

    Rename boolean options with a bang!
    Except :help and :version which really don't require any bang.

--- lib/dredd/rack/runner.rb
+++ lib/dredd/rack/runner.rb
@@ -4,8 +4,8 @@ module Dredd
 
       undef_method :method
 
-      BOOLEAN_OPTIONS         = [:dry_run, :names, :sorted, :inline_errors,
-                                 :details, :color, :timestamp, :silent,
+      BOOLEAN_OPTIONS         = [:dry_run!, :names!, :sorted!, :inline_errors!,
+                                 :details!, :color!, :timestamp!, :silent!,
                                  :help, :version]
       SINGLE_ARGUMENT_OPTIONS = [:hookfiles, :only, :reporter, :output, :header,
                                  :user, :method, :level, :path]
