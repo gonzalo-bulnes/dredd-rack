f649ad811d5a833ddcb6cdd2d3736ad5050b3b47
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-03-04 00:59:58 -0300

    Add pending specs as a roadmap

--- spec/lib/dredd/rack/runner_spec.rb
+++ spec/lib/dredd/rack/runner_spec.rb
@@ -14,6 +14,10 @@ describe Dredd::Rack::Runner do
     expect(subject).to respond_to :command_parts=
   end
 
+  it 'responds to :run'
+
+  it 'responds to :valid?'
+
   describe 'responds to all the Dredd options' do
 
     Dredd::Rack::Runner::OPTIONS.each do |option|
@@ -23,11 +27,31 @@ describe Dredd::Rack::Runner do
     end
   end
 
+  describe 'responds to the negated form of the Dredd boolean options' do
+
+    Dredd::Rack::Runner::BOOLEAN_OPTIONS.each do |option|
+      it "responds to :no_#{option}"
+    end
+  end
+
   describe '#command' do
 
     it 'defaults to "dredd"' do
       expect(subject.command).to eq 'dredd'
     end
+  end
+
+  describe '#initialize' do
+    it 'emits a warning if essential arguments are missing'
+    it 'enforces a default path to blueprints'
+  end
+
+  describe '#run' do
+    it 'runs Dredd!'
+  end
+
+  describe '#valid?' do
+    it 'requires at least a path to blueprints and an API endpoint'
   end
 
   Dredd::Rack::Runner::BOOLEAN_OPTIONS.each do |option|
