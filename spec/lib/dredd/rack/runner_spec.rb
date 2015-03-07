1f3cea1ba9f7bceeff493d7f52ae7a0aa063f82b
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-03-07 02:44:15 -0300

    Add failing specs

--- spec/lib/dredd/rack/runner_spec.rb
+++ spec/lib/dredd/rack/runner_spec.rb
@@ -14,9 +14,17 @@ describe Dredd::Rack::Runner do
     expect(subject).to respond_to :command_parts=
   end
 
-  it 'responds to :run'
+  it 'responds to :run' do
+    expect(subject).to respond_to :run
+  end
 
-  it 'responds to :valid?'
+  it 'responds to :valid?' do
+    expect(subject).to respond_to :valid?
+  end
+
+  it 'generates a valid command by default' do
+    expect(subject).to be_valid
+  end
 
   describe 'responds to all the Dredd options' do
 
@@ -36,8 +44,8 @@ describe Dredd::Rack::Runner do
 
   describe '#command' do
 
-    it 'defaults to "dredd"' do
-      expect(subject.command).to eq 'dredd'
+    it 'defaults to "dredd doc/*apib doc/*.apib.md http://localhost:3000"' do
+      expect(subject.command).to eq 'dredd doc/*.apib doc/*.apib.md http://localhost:3000'
     end
   end
 
@@ -47,11 +55,29 @@ describe Dredd::Rack::Runner do
   end
 
   describe '#run' do
-    it 'runs Dredd!'
+
+    context 'when the command is valid' do
+
+      it 'runs Dredd!' do
+        command = double()
+        allow(subject).to receive(:command).and_return(command)
+        allow(command).to receive(:valid?).and_return(true)
+
+        expect(Kernel).to receive(:system).with(command)
+        subject.run
+      end
+    end
   end
 
   describe '#valid?' do
-    it 'requires at least a path to blueprints and an API endpoint'
+
+    context 'when the generated command has less than two arguments' do
+
+      it 'returns false' do
+        allow(subject).to receive_message_chain(:command, :has_at_least_two_arguments?).and_return(false)
+        expect(subject).not_to be_valid
+      end
+    end
   end
 
   Dredd::Rack::Runner::BOOLEAN_OPTIONS.each do |option|
@@ -63,6 +89,45 @@ describe Dredd::Rack::Runner do
   Dredd::Rack::Runner::SINGLE_ARGUMENT_OPTIONS.each do |option|
     describe "##{option}" do
       it_behaves_like 'a single-argument option', option, ['first argument', 'second argument']
+    end
+  end
+end
+
+describe String do
+
+  it 'responds to :has_at_least_two_arguments?' do
+    expect(subject).to respond_to :has_at_least_two_arguments?
+  end
+
+
+  describe '#has_at_least_two_arguments?' do
+
+    context 'when the String can be interpreted as a command with at least two arguments' do
+      context 'returns true (reliable)' do
+
+        ['dredd doc/*.apib http://api.example.com',
+         'dredd doc/*.apib doc/*apib.md http://api.example.com',
+         'dredd doc/*.apib http://api.example.com --level verbose'].each do |subject|
+
+          it "e.g. '#{subject}'" do
+            expect(subject.has_at_least_two_arguments?).to be_truthy
+          end
+        end
+      end
+    end
+
+    context 'when the String can be interpreted as a command with less than two arguments' do
+      context 'returns false (unreliable)' do
+
+        ['dredd http://api.example.com',
+         'dredd doc/*.apib --dry-run',
+         'dredd --dry-run --level verbose'].each do |subject|
+
+          it "e.g. '#{subject}'" do
+            expect(subject.has_at_least_two_arguments?).to be_falsey
+          end
+        end
+      end
     end
   end
 end
