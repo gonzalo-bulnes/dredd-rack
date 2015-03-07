eab7956e65d9c858510cc610b526ef3d79c43e43
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-03-07 02:45:02 -0300

    Add Dredd::Rack::Runner#run

--- lib/dredd/rack/runner.rb
+++ lib/dredd/rack/runner.rb
@@ -16,11 +16,18 @@ module Dredd
       attr_accessor :command_parts
 
       def initialize
-        @command_parts = ['dredd']
+        @dredd_command = 'dredd'
+        @paths_to_blueprints = 'doc/*.apib doc/*.apib.md'
+        @api_endpoint = 'http://localhost:3000'
+        @command_parts = []
       end
 
       def command
-        @command_parts.join(' ')
+        ([@dredd_command, @paths_to_blueprints, @api_endpoint] + @command_parts).join(' ')
+      end
+
+      def run
+        Kernel.system(command) if command_valid?
       end
 
       def respond_to_missing?(method, include_private=false)
@@ -28,6 +35,10 @@ module Dredd
       end
 
       private
+
+        def command_valid?
+          command.has_at_least_two_arguments?
+        end
 
         def method_missing(name, *args)
           super unless OPTIONS.include? name.to_sym
@@ -39,5 +50,36 @@ module Dredd
         end
 
     end
+  end
+end
+
+class String
+
+  # Verify that a command has at least two arguments (excluding options)
+  #
+  # Examples:
+  #
+  #    "dredd doc/*.apib http://api.example.com".valid? # => true
+  #    "dredd doc/*.apib doc/*apib.md http://api.example.com".valid? # => true
+  #    "dredd doc/*.apib http://api.example.com --level verbose".valid? # => true
+  #    "dredd http://api.example.com".valid? # => false
+  #    "dredd doc/*.apib --dry-run".valid? # => false
+  #    "dredd --dry-run --level verbose".valid? # => false
+  #
+  # Known limitations:
+  #
+  # Does not support short flags. (e.g. using `-l` instead of `--level`).
+  # Requires options to be specified after the last argument.
+  #
+  # Note:
+  #
+  # The known limitations imply that there may be false negatives: this method
+  # can return false for commands that do have two arguments or more. But there
+  # should not be false positives: if the method returns true, then the command
+  # does have at least two arguments.
+  #
+  # Returns true if the command String has at least two arguments, false otherwise.
+  def has_at_least_two_arguments?
+    split('--').first.split(' ').length >= 3
   end
 end
