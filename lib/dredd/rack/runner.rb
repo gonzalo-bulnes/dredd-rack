aebdff501d377f353ff9f0e417966e18974df107
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-03-07 13:32:45 -0300

    Add Dredd::Rack::Runner command generator
    Provides a Ruby wrapper to generate Dredd commands.
    
    Usage:
    
        dredd = Dredd::Rack::Runner.new
        system(dredd.level(:silly).dry_run)

--- lib/dredd/rack/runner.rb
+++ lib/dredd/rack/runner.rb
@@ -10,6 +10,31 @@ module Dredd
       SINGLE_ARGUMENT_OPTIONS = [:hookfiles, :only, :reporter, :output, :header,
                                  :user, :method, :level, :path]
       OPTIONS = BOOLEAN_OPTIONS + SINGLE_ARGUMENT_OPTIONS
+
+      attr_accessor :command_parts
+
+      def initialize
+        @command_parts = ['dredd']
+      end
+
+      def command
+        @command_parts.join(' ')
+      end
+
+      def respond_to_missing?(method, include_private=false)
+        OPTIONS.include? method.to_sym || super
+      end
+
+      private
+
+        def method_missing(name, *args)
+          super unless OPTIONS.include? name.to_sym
+
+          option_flag = name.to_s.gsub('_', '-').prepend('--')
+          command_parts = self.command_parts.push option_flag
+          command_parts = self.command_parts.push args.slice(0).to_s if SINGLE_ARGUMENT_OPTIONS.include? name
+          self
+        end
     end
   end
 end
